2e1cc53f10674612c5c4cfa4e7e168d5e5e212cb
DuzaoOliveira <edu.oliblack@hotmail.com>
2022-07-14 11:14:09 -0300

    exercicio estrutura de dados 2

---
+++ estrutura.js
@@ -0,0 +1,55 @@
+// const boolean = true ou false 
+// const string = 'Eduardo'
+// const number = 10
+// const float = 10.1
+// const sem_resultado = undefined
+// const nula = null
+// Estrutura que comuns que englobam tudo
+// cont array = [] ----sempre que quero listar
+// const objeto = {}
+
+// 1 lista, 3 objetos, 7 dados, 3 SVGStringList, 2 boolean, 1 number, 1 dado(objeto, 2 dados, strings)
+
+const lojas = [
+    {
+        marca: 'Nike',
+        camisa: 'Adidas',
+        calca: 'Puma',
+        numero_lojas: 'true',
+        loja_mais_de_50: 'true',
+        unidades: 10,
+
+        agasalhos: {
+            conjunto: 'Moletom',
+            blusa: 'Tactel',
+        },
+    },
+
+    {
+        touca: 'Rebook',
+        bone: 'New era',
+        bandana: 'Levis',
+        numero_de_itens: 'true',
+        loja_mais_de_50: 'true',
+        unidades: 15,
+
+        acessorios: {
+            cinto: 'Couro',
+            oculos: 'Ray ban',
+        },
+    },
+
+    {
+        casual: 'Tenis Nike',
+        tenis: 'infantil',
+        sapato: 'Feminino',
+        numero_de_itens: 'true',
+        loja_mais_de_50: 'true',
+        unidades: 15,
+
+        verao: {
+            chinelo: 'Mormaii',
+            bermuda: 'Surf',
+        },
+    },
+]
