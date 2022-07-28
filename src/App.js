d96d249dc1f740b694a63d68d6ef337cd95ee4da
DuzaoOliveira <edu.oliblack@hotmail.com>
2022-07-28 17:56:44 -0300

    aula de funções

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
+import { Input, Click, Text } from './funcoes'
 
 function App() {
+  const Text = (abacaxi, bom) => {
+  }
   
-console.log(4+JSON.parse('20'))
+  const Input = (valorDigitado, nome) => {
+    console.log(valorDigitado, nome)
+  }
   return (
-    <div className='App'>
-  <form>
-    <label>Nome:</label>
-    <input onChange={()=>{alert(4+JSON.parse('20'))}}/> 
-    <div>    
-    <button onClick={()=>{alert('Eu consegui')} }>Eduardo Oliveira</button>
-    </div>
-  </form>
-    </div>
-  );
+    <form>
+      <input onChange={(e) => { Input(e.target.value, '--é muito louco--') }} />
+      <button onClick={(e) => { Click('Eduardo') }}>Enviar</button>
+      <button onClick={(e) =>{Text('abacaxi')}}>Abacaxi</button>
+    </form>
+  )
+
 }
 
 export default App;
 
+// criar uma função externa que chame o numero digitado no input no console log acrescentando
+// a mensagem --é muito louco--
+
+// passar duas variaveis para uma segunda função aonde elas retornem dentro de um alert
+// depois do click
---
+++ src/funcoes/index.jsx
@@ -0,0 +1,11 @@
+export const Input = (valorDigitado, nome, digitado, nome2) => {
+    console.log(valorDigitado, nome, nome2)
+}
+
+export const Click = (valorDigitado, nome) => {
+    alert(`o ${nome} é muito legal`)
+}
+
+export const Text = (nome2) => {
+    alert(`o${nome2} e bom` )
+}
