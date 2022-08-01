a8f5e244b3b8f58b87aaa38edc0a8d5aebe8bcde
M4rc1an0 <marciano.arantes1999@gmail.com>
2022-08-01 16:14:09 -0300

    aula de If, else

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import { Input, Click, Fruta1 } from './funcoes'
+import { Input ,Text } from './funcoes'
 
 function App() {
 
-
-  const fruta = 'abacaxi'
-  const alimento = 'bom'
-
-  const Input = (valorDigitado, nome) => {
-    console.log(valorDigitado, nome)
-  }
   return (
     <form>
-      <input onChange={(e) => { Input(e.target.value, '--é muito louco--') }} />
-      <button onClick={(e) => { Click('Eduardo') }}>Enviar</button>
-      <p onClick={(e) => { Fruta1(fruta, alimento) }}>Eduardo Oliveira</p>
+      <input onChange={(e) => { Input(e.target.value) }} />
+      <input onChange={(e) => { Text(e.target.value) }} />
+      <button>Enviar</button>
     </form>
   )
 
--- src/funcoes/index.jsx
+++ src/funcoes/index.jsx
@@ -1,14 +1,27 @@
-export const Input = (valorDigitado, nome, digitado,) => {
-    console.log(valorDigitado, nome,)
+export const Input = (digitando) => {
+    if (digitando === '--Guitarra--') {
+        console.log('tem cordas')
+    }
+    else if (digitando === '--Bateria--') {
+        console.log('tem pedal')
+    }
+    else if (digitando === '--Microfone--') {
+        console.log('tem fios')
+    }
+
+    else {
+        console.log('não existe esse instrumento')
+    }
 }
 
-export const Click = (nome) => {
-    alert(`o ${nome} é legal`)
-
-}
-
-export const Fruta1 = (fruta, alimento) => {
-    alert(`O ${fruta} é ${alimento}`)
+export const Text = (digitado) => {
+    switch (digitado) {
+        case 'Eduardo': console.log('gosta de futebol'); break;
+        case 'Felipe': console.log('gosta de anime'); break;
+        case 'Rogerio': console.log('gosta de game'); break;
+        default:
+            console.log('não atende nenhuma das opções ')
+    }
 
 }
 
