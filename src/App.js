7f52c229ef4fb37baa05c5c4d6c35f5a7a0c6598
M4rc1an0 <marciano.arantes1999@gmail.com>
2022-08-04 15:17:30 -0300

    aula corrigida onClick

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 
 function App() {
-  
-console.log(4+JSON.parse('20'))
-  return (
-    <div className='App'>
-  <form>
-    <label>Nome:</label>
-    <input onChange={()=>{alert(4+JSON.parse('20'))}}/> 
-    <div>    
-    <button onClick={()=>{alert('Eu consegui')} }>Eduardo Oliveira</button>
-    </div>
-  </form>
-    </div>
-  );
+
+    const number = 4
+    const teste = 'Eu consegui'
+    return (
+        <div className='App'>
+            <form>
+                <label>Nome:</label>
+                <input onChange={(e) => { alert(JSON.parse(e.target.value) + number) }} />
+                <div>
+                    <button onClick={() => { alert(teste) }}>Eduardo Oliveira</button>
+                </div>
+            </form>
+        </div>
+    );
 }
+
 
 export default App;
 
