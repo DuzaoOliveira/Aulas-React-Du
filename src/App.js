a6e583dd59a3574a8e2310370178d7e2a19801ee
m4rc1an0m <marciano.arantes1999@gmail.com>
2022-08-16 13:01:21 -0300

    conexoes-get

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
-import RoutesConfig from './routes';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css"
 
 
-function App () {
+function App() {
 
-    return(
-        <RoutesConfig/>
+    const [resposta, setResposta, palmeiras] = useState()
 
+
+    // fetch('https://teste-aula-metodos-90a63-default-rtdb.firebaseio.com/clubes.json')
+    // .then(response => response.json())
+    // .then(data => console.log(data));
+    useEffect(() => {
+        axios.get('https://teste-aula-metodos-90a63-default-rtdb.firebaseio.com/futebol.json')
+            .then(function (response) {
+                setResposta(response.data);
+                console.log(response.data)
+            })
+
+    }, [])
+
+
+
+    return (
+
+        <div className="clubes">
+            <h1>Clubes</h1>
+            {resposta &&
+                <>
+                    {
+                        Object.values(resposta).map(times => {
+                            return (
+                                <>
+                                    <p> {times.clube}</p>
+                                    <p> {times.jogador}</p>
+                                    <p> {times.camisa}</p>
+                                    <p> {times.idade}</p>
+                                    <p> {times.altura}</p>
+                                    <p> {times.nacionalidade}</p>
+                                  
+                                </>
+                            )
+                        })}
+                </>
+            }
+        </div>
     )
 }
 
